e02c1a86f246c22e1c33616b1542202ea5f37c0e
aritra <aritra>
2008-05-14 14:11:04 +0000

    ...
    git-svn-id: file:///Users/simon/Workspaces/WorkspaceSVN/ISIP_svn/svn/simonsomapdev/trunk@58 86bdb08a-1140-0410-90b1-c6a12c039488

--- ldk5912/gpp_side/wav_rf6_gpp/wav_rec_rf6_gpp/rf6/Linux/OMAP/mainprog.c
+++ ldk5912/gpp_side/wav_rf6_gpp/wav_rec_rf6_gpp/rf6/Linux/OMAP/mainprog.c
@@ -10,7 +10,7 @@
 #include <sys/ioctl.h>
 
 /*--------------------------------------------- Linux specific include */
-//#include <linux/soundcard.h> blaaaaaaa
+//#include <linux/soundcard.h> blaaaaaaaaaa
 
 /*  ----------------------------------- DSP/BIOS Link                   */
 #include <gpptypes.h>
